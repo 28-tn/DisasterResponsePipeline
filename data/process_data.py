fcd7bf17eb8d7b0b35348d862ea39684a8c9d750
28-tn <tobias.noesselt@gmx.de>
2020-05-01 16:09:52 +0200

    built ETL pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,116 @@
 import sys
-
+import pandas as pd
+import sqlite3
+import numpy as np
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Reads in messages and categorie data from corresponding csv files,
+    merges the data to a single dataframe and returns the dataframe.
 
+    Parameters
+    ----------
+    messages_filepath : STRING
+        path to csv file with messages-data
+    categories_filepath : STRING
+        path to csv file with category-data
+
+    Returns
+    -------
+    df: Pandas.DataFrame
+        dataframe with merged data from messages and categories datasets
+    """
+    
+    # Read in data
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    
+    # Merge data
+    df = messages.merge(categories, on='id', how='right')
+    
+    # Split categories into separate category columns
+    category_columns = df['categories'].str.split(';', expand=True)
+    category_colnames = category_columns.loc[0,:].str.split('-', expand=True).loc[:,0]
+    category_columns.columns = category_colnames
+    
+    # Convert category values to numbers (0 or 1)
+    for column in category_columns:
+        # set each value to be the last character of the string
+        category_columns[column] = category_columns[column].str[-1:]
+        
+        # convert column from string to numeric
+        category_columns[column] = category_columns[column].apply(lambda x: int(x))
+    
+    # Concatenate the new columns to df and drop the old categories column
+    df = df.drop(columns='categories')
+    df = pd.concat([df, category_columns], axis=1)
+    
+    return df
 
 def clean_data(df):
-    pass
+    """
+    Cleans the data (removes duplicates)
+
+    Parameters
+    ----------
+    df : Pandas.DataFrame
+        Dataset to be cleaned
+
+    Returns
+    -------
+    df : Pandas.DataFrame
+        Cleaned dataset
+
+    """
+    
+    # Drop duplicates
+    old_lines = df.shape[0]
+    df = df.drop_duplicates(subset='id')
+    new_lines = df.shape[0]
+    print('Removed {} duplicate rows'.format(old_lines-new_lines))
+    
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    
+    # Connect to database
+    con = sqlite3.connect(database_filename)
+    cur = con.cursor()
+    
+    # Drop table if it exists
+    cur.execute("DROP TABLE IF EXISTS response_data;")
+    
+    # Create new table
+    sql_types = {np.dtype('object'): 'TEXT', np.dtype('int64'): 'INT'} # translates dtype to sql type
+    create_cmd = 'CREATE TABLE response_data ('
+    for col in df.columns:
+        create_cmd += col+' '+sql_types[df.dtypes[col]]+', '
+    create_cmd += 'PRIMARY KEY(id));'
+    cur.execute(create_cmd)
+    
+    # Prepare  INSERT command
+    insert_cmd = 'INSERT INTO response_data ('
+    for col in df.columns:
+        insert_cmd += col+', '
+    insert_cmd = insert_cmd[:-2] # get rid of the last comma+space
+    insert_cmd += ') VALUES ('
+    for col in df.columns:
+        insert_cmd += '?, '
+    insert_cmd = insert_cmd[:-2] # get rid of the last comma+spac
+    insert_cmd += ');'
+    
+    # Iter rows and insert data
+    for index, data in df.iterrows():
+        try:
+            cur.execute(insert_cmd, data)
+        except sqlite3.OperationalError:
+            print('SQL error on:')
+            print(insert_cmd)
+    
+    # Commit changes and close connection
+    con.commit()
+    con.close()
 
 
 def main():
